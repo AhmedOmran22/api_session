334ee0a55b10b7152e8a8d210c1170e666d1ed1d
Ahmed Omran <mamdouhanahamada@gmail.com>
2025-07-12 16:18:44 +0300

    delet unused file and make commit to test that git and git hub work succeflly

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'api_session.dart';
-
+// test git after clone api session repo from github
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
